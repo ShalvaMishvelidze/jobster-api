b98cb6c7b14e3289545e55b11cc719b9115c4aae
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2024-02-09 18:55:38 +0400

    basic config

---
+++ app.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+// routes
+app.get('/', (req, res) => {
+	res.send('jobs api');
+});
+
+const port = process.env.PORT || 3535;
+
+const start = async () => {
+	try {
+		app.listen(port, () =>
+			console.log(`Server is listening on port ${port}...`)
+		);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+start();
