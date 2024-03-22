db3b836e8d8fffda00a585fa8ea99c398998c751
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2024-03-22 20:11:02 +0400

    added some additional fields to the job

--- models/Job.js
+++ models/Job.js
@@ -1,38 +1,49 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const JobSchema = new mongoose.Schema(
-	{
-		company: {
-			type: String,
-			required: [true, 'Please provide company name'],
-			maxlength: 50,
-		},
-		position: {
-			type: String,
-			required: [true, 'Please provide position'],
-			maxlength: 100,
-		},
-		status: {
-			type: String,
-			enum: ['interview', 'declined', 'pending'],
-			default: 'pending',
-		},
-		createdBy: {
-			type: mongoose.Types.ObjectId,
-			ref: 'User',
-			required: [true, 'Please provide user'],
-		},
-		questions: {
-			type: [
-				{
-					question: { type: String, required: true },
-					answer: { type: String, required: false },
-				},
-			],
-			required: false,
-		},
-	},
-	{ timestamps: true }
+  {
+    company: {
+      type: String,
+      required: [true, "Please provide company name"],
+      maxlength: 50,
+    },
+    position: {
+      type: String,
+      required: [true, "Please provide position"],
+      maxlength: 100,
+    },
+    status: {
+      type: String,
+      enum: ["interview", "declined", "pending"],
+      default: "pending",
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please provide user"],
+    },
+    jobType: {
+      type: String,
+      enum: ["full-time", "part-time", "remote", "internship"],
+      default: "full-time",
+    },
+    jobLocation: {
+      type: String,
+
+      default: "my city",
+      required: true,
+    },
+    questions: {
+      type: [
+        {
+          question: { type: String, required: true },
+          answer: { type: String, required: false },
+        },
+      ],
+      required: false,
+    },
+  },
+  { timestamps: true }
 );
 
-module.exports = mongoose.model('Job', JobSchema);
+module.exports = mongoose.model("Job", JobSchema);
