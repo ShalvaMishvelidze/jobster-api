32777272bc876957234f11afaa9c44f0ade12036
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2024-03-22 13:23:46 +0400

    removed cors middleware to restrict access

--- app.js
+++ app.js
@@ -13,12 +13,10 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 
 // extra security packages
 const helmet = require("helmet");
-const cors = require("cors");
 const xss = require("xss-clean");
 
 app.use(express.json());
 app.use(helmet());
-app.use(cors());
 app.use(xss());
 
 // routes
