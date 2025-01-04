afa99f77bba09bfdc96f15f4a9e80a8143220122
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-01-04 15:05:25 +0400

    changed port for convenience

--- app.js
+++ app.js
@@ -1,52 +1,52 @@
-require("dotenv").config();
-require("express-async-errors");
+require('dotenv').config();
+require('express-async-errors');
 
-const path = require("path");
-const express = require("express");
+const path = require('path');
+const express = require('express');
 const app = express();
-const authRouter = require("./routes/auth");
-const jobsRouter = require("./routes/jobs");
-const connectDB = require("./db/connect");
-const authenticateUser = require("./middleware/authentication");
+const authRouter = require('./routes/auth');
+const jobsRouter = require('./routes/jobs');
+const connectDB = require('./db/connect');
+const authenticateUser = require('./middleware/authentication');
 
 // error handlers
-const notFoundMiddleware = require("./middleware/not-found");
-const errorHandlerMiddleware = require("./middleware/error-handler");
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 // extra security packages
-const helmet = require("helmet");
-const xss = require("xss-clean");
+const helmet = require('helmet');
+const xss = require('xss-clean');
 
-app.set("trust proxy", 1);
+app.set('trust proxy', 1);
 
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.resolve(__dirname, './client/build')));
 app.use(express.json());
 app.use(helmet());
 app.use(xss());
 
 // routes
-app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/jobs", authenticateUser, jobsRouter);
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
 });
 
 // error handlers
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URI);
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		await connectDB(process.env.MONGO_URI);
+		app.listen(port, () =>
+			console.log(`Server is listening on port ${port}...`)
+		);
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 start();
